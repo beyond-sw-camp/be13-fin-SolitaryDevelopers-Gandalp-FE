9ceeaa1863668b4baee5d2df014598df3977f76b
limhyunjo <sibagyeoni@gmail.com>
2025-06-05 09:21:12 +0900

    [임현조] 지도 프론트 수정

--- src/views/hospital/HospitalMap.vue
+++ src/views/hospital/HospitalMap.vue
@@ -3,7 +3,7 @@
 
 
     <MapSidebar
-      :hospitals = "hospitals"
+      :hospitals = "sortedHospitals"
       :total-pages="totalPages"
       :current-page="currentPage"
       :sort-by="sortBy"
@@ -26,28 +26,28 @@
           v-model="searchKeyword"
           @keyup.enter="onSearch(searchKeyword)"
           type="text"
-          placeholder="빠른 검색 결과를 위해 의료기관 이름을 최대한 정확하게 검색해주세요"
+          placeholder="빠른 검색 결과를 위해 의료기관 이름을 정확하게 검색해주세요"
         />
 
         <button class="btn-search" @click="onSearch(searchKeyword)">
           검색</button>
       </div>
 
-     <!-- 지도 ( 현재 위치 조회가 느리면 스피너로 로딩 화면 보여줌 )-->
 
       <div class="map-wrapper">
-        <!-- Map 컴포넌트: userPosition이 세팅되면 실제 지도를 랜더링 -->
         <Map
-             v-if="userPosition.lat !== null"
-             ref="mapRef"
-             :hospitals="hospitals"
-             :center-lat="userPosition.lat"
-             :center-lng="userPosition.lon"
-           />
+          v-if="userPosition.lat !== null"
+          ref="mapRef"
+          :hospitals="hospitals"
+          :center-lat="userPosition.lat"
+          :center-lng="userPosition.lon"
+          :auto-fit-bounds="false"
+          :show-current="!isSearching"
+        />
 
       </div>
 
-<!--      아이콘 저작권 표시 -->
+      <!--      아이콘 저작권 표시 -->
       <div class="icon-attribution">
         아이콘 제작자
         <a
@@ -56,6 +56,20 @@
           rel="noopener noreferrer"
         >
           I3oundless
+        </a>,
+        <a
+          href="https://www.flaticon.com/authors/pixel-perfect"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pixel perfect
+        </a>,
+        <a
+          href="https://www.flaticon.com/authors/meaicon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          meaicon
         </a>
         from
         <a
@@ -81,10 +95,9 @@ import Header from "@/components/common/Header.vue";
 import apiClient from "@/api/axios.js";
 
 //  네이버 내비/지도 앱 방식 반영
-//  소수점 5자리(≈1.1m) 반올림 헬퍼: GPS 흔들림(±0.5m) 방지
-//  첫 로딩 시 Last Known Location 활용: onMounted에서 한 번만 위치 얻기
+//  소수점 5자리(≈1.1m) 반올림 : GPS 흔들림(±0.5m) 방지
+//  첫 로딩 시 마지막 위치 활용: onMounted에서 한 번만 위치 얻기
 //  이후 “현재 위치 찾기” 시, 이미 userPosition이 있으면 GPS 재호출 없이 fetchHospitals()
-//  getCurrentPosition 옵션: enableHighAccuracy=false, timeout=5000, maximumAge=60000
 
 
 // 소수점 5자리까지 반올림 (≈1.1m 단위)
@@ -100,9 +113,24 @@ const currentPage = ref(0)
 const pageSize = 10
 const totalPages = computed(() => Math.ceil(hospitals.value.length/ pageSize))
 const mapRef = ref(null)
-const sidebarRef = ref(null)
+
 // 전달할 병원 리스트
 const hospitals = ref([])
+
+// 서버에서 받아온 데이터 정렬
+const sortedHospitals = computed(() => {
+  const arr = [...hospitals.value]
+  if(sortBy.value === 'DISTANCE') {
+    arr.sort((a, b) => {
+      if(!Number.isFinite(a.distanceKm)) return 1
+      if(!Number.isFinite(b.distanceKm))return -1
+        return a.distanceKm - b.distanceKm
+    })
+  } else {
+    arr.sort((a, b) => b.availableErCount - a.availableErCount)
+  }
+  return arr
+})
 // 현재 위치(반올림된 값)
 const userPosition = reactive({lat: null, lon: null})
 // “마지막으로 서버에 병원 조회 요청을 보냈던” 위치를 저장
@@ -114,7 +142,7 @@ const isLoading = ref(false)
 // 현재 위치 조회 중 상태
 const isLocating = ref(false)
 
-
+const isSearching = ref(false)
 
 
 // Haversine 공식: 두 위경도 사이의 거리(m) 계산
@@ -170,24 +198,6 @@ function trySendLocation(lat, lon) {
   }
 }
 
-// 수정하기 전의 stomp 요청 ( 10초가 지났거나 500m 이상 움직이면 보냄 )
-// function trySendLocation(lat, lon) {
-//   const now = Date.now()
-//   const dist = lastSent.lat !== null? calcDistance(lastSent.lat, lastSent.lon, lat, lon)
-//     : Infinity
-//
-//   if(now - lastSent.time > SEND_INTERVAL || dist > SEND_DISTANCE) {
-//     stompClient.value.publish({
-//       destination: '/publish/location',
-//       headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') },
-//       body: JSON.stringify({ lat, lon })
-//     })
-//     lastSent = {lat, lon, time : now}
-//   }
-// }
-
-//-------------------------------------------------------------------
-// 현재 위치 기반 조회 (rest)
 
 // 현재 위치 기반 데이터 조회 ( 페이지 첫 랜더링 시 보여줄 화면 데이터 )
 async function fetchHospitals() {
@@ -211,8 +221,8 @@ async function fetchHospitals() {
           lon: userPosition.lon,
           sortOption: sortBy.value,
           keyword: searchKeyword.value ? searchKeyword.value : undefined,
-          page : currentPage.value,
-          size : pageSize
+          // page : currentPage.value,
+          // size : pageSize
         }
       }
     )
@@ -228,6 +238,9 @@ async function fetchHospitals() {
     // 조회 후 lastPosition 업데이트: 마지막으로 조회한 위치 기록
     lastPosition.lat = userPosition.lat
     lastPosition.lon = userPosition.lon
+
+    // 받은 후 현재 페이지를 0으로 초기화 후 페이징 정렬 적용
+    currentPage.value = 0
   }catch(err) {
     console.error('[fetchHospitals] 에러', err.response?.data || err.message)
     hospitals.value  = []
@@ -236,6 +249,7 @@ async function fetchHospitals() {
     isLoading.value = false
   }
 }
+
 
 
 function updateHospitalInList(updatedHospital) {
@@ -317,19 +331,19 @@ function connectWebSocket() {
 // 현재 위치 찾기 버튼
 // 사용자 위치 정보 허용
 // 초기 위치 조회 및 전송 + rest 호출
+// wifi로 얻어오는거 실패하면 gps로 얻어오는거 추가함
 async function onLocateMe() {
-  // 버튼을 비활성화
-  isLocating.value = true
-  searchKeyword.value = ''
-  currentPage.value = 0
+    // 버튼을 비활성화
+    isLocating.value = true
+    searchKeyword.value = ''
+    currentPage.value = 0
 
-
-  console.log('[locateMe] 호출됨')
-  if( !('geolocation' in navigator)){
-    alert('브라우저에서 Geolocation을 지원하지 않습니다.')
-    isLocating.value = false;
-    return
-  }
+    console.log('[locateMe] 호출됨')
+    if( !('geolocation' in navigator)){
+      alert('브라우저에서 Geolocation을 지원하지 않습니다.')
+      isLocating.value = false;
+      return
+    }
 
     // 이미 위치가 세팅되어 있으면, GPS 재호출 없이 바로 병원 조회
     if (userPosition.lat !== null && userPosition.lon !== null) {
@@ -338,98 +352,136 @@ async function onLocateMe() {
       return
     }
 
+    // 위치가 있는 경우 gps 호출 없이 stomp로 위치 보냄
+    if (stompClient.value && stompClient.connected) {
+      stompClient.value.publish({
+        destination: '/publish/location',
+        headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') },
+        body: JSON.stringify({ lat: userPosition.lat, lon: userPosition.lon })
+      })
+      lastSent = { lat: userPosition.lat, lon: userPosition.lon, time: Date.now() }
+   }
+
   // 위치 없는 경우 최초 한번만 실행
-    navigator.geolocation.getCurrentPosition(
+  navigator.geolocation.getCurrentPosition(
 
-      async ({coords}) => {
+    async ({coords}) => {
 
-        // 검색창 비우기
-        searchKeyword.value = ''
-        // 페이지 0으로 초기화
-        currentPage.value = 0
-
-        // 정렬 기준은 초기화 할까 말까...
-        //sortBy.value = 'DISTANCE'
-
-        // 유저 위치 ( gps 좌표를 소수점 5자리로 반올림 )
-        userPosition.lat = roundToFiveDecimal(coords.latitude);
-        userPosition.lon = roundToFiveDecimal(coords.longitude);
-
-        // 최초 한 번 rest로 데이터 받아옴
-        await fetchHospitals('')
-
-        // 버튼 다시 활성화
-        isLocating.value = false
-
-        // rest api 후에 stomp(실시간 업데이트) 연결
-        connectWebSocket()
-
-        // 최초 위치도 stomp로 보냄
-        // 나의 위치를 서버로 한 번 더 전송 ( 서버가 distance 업데이트를 날려주도록
-        if(stompClient.value && stompClient.value.connected) {
-
-          stompClient.value.publish({
-            destination: '/publish/location',
-            headers: {Authorization: 'Bearer ' + localStorage.getItem('accessToken')},
-            body: JSON.stringify({lat: userPosition.lat, lon: userPosition.lon})
-          })
-
-        }
-
-      },
-      (err) => {
-        console.error('초기 위치 조회 실패:', err)
-        alert("위치 정보를 가져오지 못했습니다.")
-        isLocating.value = false
-      },
-      {
-        enableHighAccuracy: false, // 네이버 권장: Wi-Fi/기지국 우선 → 빠른 응답
-        timeout: 5000, // 5초 이상 걸리면 에러
-        maximumAge: 60000, // 1분 이내 캐시된 위치가 있으면 재사용
-      }
-    )
-
-  }
-
-
-  // 검색 버튼 혹은 엔터 키 누른 경우
-  async function onSearch(keyword) {
-    searchKeyword.value = keyword.trim()
-    currentPage.value = 0
-    await fetchHospitals()
-  }
-
-  // 정렬 기준
-  async function onChangeSort(option) {
-
-    if(['DISTANCE', 'ER_COUNT'].includes(option)) {
-      sortBy.value = option
+      // 검색창 비우기
+      searchKeyword.value = ''
+      // 페이지 0으로 초기화
       currentPage.value = 0
-      await fetchHospitals()
-    }
 
+      // 정렬 기준은 초기화 할까 말까...
+      //sortBy.value = 'DISTANCE'
+
+      // 유저 위치 ( gps 좌표를 소수점 5자리로 반올림 )
+      userPosition.lat = roundToFiveDecimal(coords.latitude);
+      userPosition.lon = roundToFiveDecimal(coords.longitude);
+
+      // 최초 한 번 rest로 데이터 받아옴
+      await fetchHospitals('')
+
+      // gps로 현재 위치를 얻은 경우
+      if (stompClient.value && stompClient.value.connected) {
+        stompClient.value.publish({
+          destination: '/publish/location',
+          headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') },
+          body: JSON.stringify({ lat: userPosition.lat, lon: userPosition.lon })
+        })
+        lastSent = { lat: userPosition.lat, lon: userPosition.lon, time: Date.now() }
+      }
+
+      // 버튼 다시 활성화
+      isLocating.value = false
+
+      // rest api 후에 stomp(실시간 업데이트) 연결
+      connectWebSocket()
+
+
+    },
+    async(err) => {
+      // wifi로 가져오는게 실패한 경우
+      if (err.code === 3) {
+        navigator.geolocation.getCurrentPosition(
+          async ({coords}) => {
+            userPosition.lat = roundToFiveDecimal(coords.latitude)
+            userPosition.lon = roundToFiveDecimal(coords.longitude)
+            await fetchHospitals()
+
+            if (stompClient.value && stompClient.value.connected) {
+              stompClient.value.publish({
+                destination: '/publish/location',
+                headers: { Authorization: 'Bearer ' + localStorage.getItem('accessToken') },
+                body: JSON.stringify({ lat: userPosition.lat, lon: userPosition.lon })
+              })
+              lastSent = { lat: userPosition.lat, lon: userPosition.lon, time: Date.now() }
+            }
+
+            connectWebSocket()
+            isLocating.value = false
+          },
+          (highErr) => {
+            // gps도 실패한 경우
+            console.error('[onLocateMe] high-accuracy 실패 코드:', highErr.code, '메시지:', highErr.message)
+            alert('GPS에서도 위치를 찾을 수 없습니다. 권한을 확인해주세요.')
+            isLocating.value = false
+          },
+          {enableHighAccuracy: true, timeout: 100000, maximumAge: 0}
+        )
+      } else {
+        if (err.code === 1) {
+          alert('위치 권한이 거부되었습니다. 브라우저 설정에서 허용해주세요')
+        } else {
+          alert('위치 정보를 가져오지 못했습니다. 다시 시도해주세요.')
+        }
+        isLocating.value = false
+      }
+    },
+    {enableHighAccuracy: false, timeout: 15000, maximumAge: 0 }
+  )
+
+}
+
+
+// 검색 버튼 혹은 엔터 키 누른 경우
+async function onSearch(keyword) {
+  isSearching.value = true
+  searchKeyword.value = keyword.trim()
+  currentPage.value = 0
+  await fetchHospitals()
+
+  isSearching.value =false
+}
+
+// 정렬 기준
+async function onChangeSort(option) {
+
+  if(['DISTANCE', 'ER_COUNT'].includes(option)) {
+    sortBy.value = option
+    currentPage.value = 0
+    //await fetchHospitals()
   }
 
-  // 페이지 바꿀 때
-  async function onChangePage(page) {
+}
+
+// 페이지 바꿀 때
+async function onChangePage(page) {
   if(page < 0 || page >= totalPages.value) return
-    currentPage.value = page
-    await fetchHospitals()
+  currentPage.value = page
+  //await fetchHospitals()
+}
+
+// 사이드바 목록 클릭하면 지도 포커스 이동
+function onSelectHospital(hospital) {
+  // 부모가 받은 병원 데이터를 지도로 전달
+
+  if (mapRef.value) {
+    mapRef.value.centerOn(hospital.latitude, hospital.longitude)
+    mapRef.value.openInfoWindow(hospital)
   }
+}
 
-  // 사이드바 목록 클릭하면 지도 포커스 이동
-  function onSelectHospital(hospital) {
-    // 부모가 받은 병원 데이터를 지도로 전달
-
-    if (mapRef.value) {
-      mapRef.value.centerOn(hospital.latitude, hospital.longitude)
-      mapRef.value.openInfoWindow(hospital)
-    }
-  }
-
-  // onMounted(async () => {
-  //   await onLocateMe()
-  // })
 
 onMounted(async () => {
   // 앱 구동 시, 마지막에 캐시된 위치(1분 이내 있으면)나 빠른 Wi-Fi 기반 위치를 얻어옴
@@ -440,7 +492,7 @@ onMounted(async () => {
         navigator.geolocation.getCurrentPosition(
           resolve,
           reject,
-          { enableHighAccuracy: false, timeout: 5000, maximumAge: 60000 }
+          { enableHighAccuracy: true, timeout: 300000, maximumAge: 60000 }
         )
       })
       userPosition.lat = roundToFiveDecimal(pos.coords.latitude)
@@ -456,67 +508,6 @@ onMounted(async () => {
   }
 })
 
- // watch(
- //   () => [userPosition.lat, userPosition.lon],
- //   async ([lat, lon]) => {
- //     if(lat != null && lon != null) {
- //       currentPage.value = 0
- //       await fetchHospitals()
- //     }
- //   }
- // )
-
-// function searchHospital() {
-//
-//   if(!sidebarRef.value) return
-//   sidebarRef.value.fetchHospitals(searchQuery.value).then(() => {
-//    // fetchHospitals가 끝난 뒤 사이드바의 hospitals를 복사
-//     hospitals.value = sidebarRef.value.hospitals.value
-//   })
-//
-// }
-//
-// // 현재 위치 찾기 버튼 누른 경우
-// async function onFindLocation({lat, lon}) {
-//
-//   // 검색창 비우기
-//   searchQuery.value=''
-//
-//   // 사이드바 내부 검색어도 클리어 & 페이지 초기화
-//   if(sidebarRef.value) {
-//     // sidebarRef.value.searchKeyword.value = ''
-//     // sidebarRef.value.currentPage.value = 0
-//
-//     // 현재 위치를 기준으로 다시 조회
-//     // await sidebarRef.value.fetchHospitals('')
-//     // hospitals.value = sidebarRef.value.hospitals.value
-//     await sidebarRef.value.locateMe()
-//     hospitals.value = [...sidebarRef.value.hospitals.value]
-//     }
-//
-//   if(mapRef.value) {
-//     mapRef.value.centerOn(lat, lon)
-//   }
-// }
-//
-// // function onSelectHospital(h) {
-// //   // 지도를 해당 병원 위치로 이동
-// //   if(mapRef.value) {
-// //     mapRef.value.centerOn(h.latitude, h.longitude)
-// //   }
-// // }
-//
-//
-// // 사이드바에서 병원 정보 클릭
-// function onSelectHospital(h) {
-//   if (mapRef.value) {
-//     //  지도를 해당 병원의 위/경도로 이동
-//     mapRef.value.centerOn(h.latitude, h.longitude)
-//
-//     //  해당 병원의 InfoWindow를 연다
-//     mapRef.value.openInfoWindow(h)
-//   }
-// }
 </script>
 
 <style scoped>
@@ -569,11 +560,11 @@ onMounted(async () => {
 }
 
 .map-wrapper {
+  margin-left: 107px;
   position: relative;
   flex: 1;
 
 }
-
 
 
 #map {
--- src/views/hospital/MapSidebar.vue
+++ src/views/hospital/MapSidebar.vue
@@ -26,14 +26,18 @@
 
     <div class="hospital-list-wrapper">
       <!--  로딩 중 -->
-<!--      <div v-if="isLoading" class="loader-container">-->
-<!--        <v-progress-circular indeterminate color="gray" size="36" />-->
+      <div v-if="isLoading" class="loader-container">
+        <v-progress-circular
+          indeterminate
+          color="gray"
+          size="36"
+          style=" position: absolute; top: 50%; transform: translate(-50%, -50%); z-index: 9999" />
 <!--        <p class="loader-text">데이터를 불러오고 있습니다...</p>-->
-<!--      </div>-->
+      </div>
 
       <!-- 로딩 끝났으나 hospitals 배열이 비어있다면 -->
       <div v-if="!isLoading && hospitals.length === 0" class="loader-container">
-        <p class="loader-text">주변 병원이 없습니다</p>
+        <p class="loader-text">해당 병원이 없습니다</p>
       </div>
 
     <!-- 병원 리스트 props.hospitals에서 가져옴 -->
@@ -44,10 +48,12 @@
         @click="$emit('select-hospital', h)"
       >
         <h5 class="name">{{ h.name }}</h5>
-        <p class="addr">📍 {{ h.address }}</p>
+        <p class="addr">
+          <img :src="hospitalPinSideUrl" style="width: 17px; height: 15px;"/>
+          {{ h.address }}</p>
         <p class="phone">
-<!--          <v-icon set="flaticon" icon="fi fi-tr-emergency-call" class="phone-icon" />-->
-          📞 {{ h.phoneNumber }}</p>
+          <img :src="hospitalPhoneUrl" style="width: 14px; height: 14px; margin-right:5px;"/>
+          {{ h.phoneNumber }}</p>
         <p class="beds"> 가용 병상 수 {{ h.availableErCount }} / 전체 {{ h.totalErCount }}</p>
       <!--   NaN인 경우 - 대기 표시 -->
         <p class="km">
@@ -97,6 +103,7 @@
 
 <script setup>
 import { computed } from 'vue'
+import hospitalPinSideUrl from '@/api/icons/hospital-pin-red-sidebar.png'
 import hospitalPhoneUrl from '@/api/icons/hospital-phone.png'
 
 const SORT_OPTIONS = ['DISTANCE', 'ER_COUNT']
@@ -165,348 +172,7 @@ function formatDuration(ms) {
 
 defineEmits(['locate-me', ' search', 'change-sort', 'select-hospital', 'change-page'])
 
-// const stompClient = ref(null)
-//
-// // 위치 전송을 위한 상태
-// let lastSent = {lat: null, lon: null, time: 0}
-// const SEND_INTERVAL = 10_000 // 10초
-// const SEND_DISTANCE = 500 // 500m
-//
-//
-//
-//
-//
-// const emit = defineEmits(['select-hospital', 'find-location'])
-//
-// // 정렬 옵션
-// const SORT_OPTIONS = ['DISTANCE', 'ER_COUNT']
-//
-// const hospitals = ref([]) // 서버에서 받은 전체 데이터
-// const sortBy = ref('DISTANCE')
-// const position = ref({ lat: null, lon: null })
-// const totalPages = computed(() => Math.ceil(hospitals.value.length/ pageSize.value))
-// const currentPage = ref(0)
-// const pageSize = ref(10)
-// const searchKeyword = ref('') // 검색어 관리
-// const token = localStorage.getItem('accessToken')
-//
-//
-// // Haversine 거리 계산 함수 ( 이런 작은 계산에서는 하버사인 거리 계산 코드를 직접 씀)
-// function calcDistance(lat1, lon1, lat2, lon2) {
-//   const toRad = d => d * Math.PI / 180
-//   const R = 6371e3
-//   const φ1 = toRad(lat1), φ2 = toRad(lat2)
-//   const Δφ = toRad(lat2 - lat1), Δλ = toRad(lon2 - lon1)
-//   const a = Math.sin(Δφ/2)**2 + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2)**2
-//   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-//   return R * c
-// }
-//
-// // 위치 전송 조건
-// function trySendLocation(lat, lon) {
-//   const now = Date.now()
-//   const dist = lastSent.lat !== null
-//     ? calcDistance(lastSent.lat, lastSent.lon, lat, lon)
-//     : Infinity
-//
-//   if(now - lastSent.time > SEND_INTERVAL || dist > SEND_DISTANCE) {
-//
-//     stompClient.value.publish({
-//       destination : '/publish/location',
-//       headers: { Authorization: 'Bearer ' + token },
-//       body: JSON.stringify({lat, lon})
-//     })
-//
-//     console.log(
-//       `[STOMP] 위치 전송: lat=${lat.toFixed(6)}, lon=${lon.toFixed(6)}, ` +
-//       `interval=${now - lastSent.time}ms, dist=${dist.toFixed(1)}m`
-//     )
-//
-//
-//     lastSent = {lat, lon, time : now}
-//
-//   }
-// }
-//
-//
-//
-// // 서버에서 받은 병원 데이터를 한 번 더 검사해서 다시 정렬
-// // 화면에 제대로 뿌리기 위해 검사
-// const sortedHospitals = computed(() => {
-//   let list = [...hospitals.value]
-//   if(sortBy.value === 'ER_COUNT' ) {
-//     list.sort((a, b) => b.availableErCount - a.availableErCount)
-//   } else {
-//     list.sort((a, b) => {
-//       // a.distanceKm, b.distanceKm가 둘 다 유효한 숫자여야 비교
-//       if (!Number.isFinite(a.distanceKm)) return 1
-//       if (!Number.isFinite(b.distanceKm)) return -1
-//       return a.distanceKm - b.distanceKm
-//     })
-//   }
-//   const start = currentPage.value * pageSize.value
-//
-//   return list.slice(start, start + pageSize.value)
-// })
-//
-//
-// const formatDuration = (ms) => {
-//   const totalSec = Math.round(ms/1000)
-//   const hours = Math.floor(totalSec / 3600)
-//   const minutse = Math.floor((totalSec % 3600)/ 60)
-//
-//   if(hours === 0) {
-//      // 1시간 이하
-//     return `${Math.ceil(totalSec / 60)} 분`
-//   }
-//
-//   const hh = String(hours).padStart(2,'0' )
-//   const mm = String(minutse).padStart(2,'0' )
-//
-//   return `${hh}시간 ${mm}분`
-// }
-//
-// // 위치가 정해지면 API 호출
-// watch(
-//   [() => position.value.lat, () => position.value.lon, () => sortBy.value, () => searchKeyword.value],
-//   ([lat, lon]) => {
-//     if(lat != null && lon != null) {
-//       fetchHospitals()
-//     }
-//   }
-// )
-//
-// onMounted(() => {
-//   locateMe()
-// })
-//
-//
-//
-//
-// // stomp 연결 및 subscribe  설정
-// const connectWebSocket = () => {
-//   const socket = new SockJS('http://localhost:8080/connect')
-//   stompClient.value = Stomp.over(socket)
-//
-//   stompClient.value.connect(
-//     {
-//       Authorization: 'Bearer ' + localStorage.getItem('accessToken')
-//     },
-//     () => {
-//       console.log('✅ STOMP 연결됨')
-//
-//
-//       // 1:1 응답을 받을 구독 경로
-//        stompClient.value.subscribe(
-//           '/user/queue/near-hospitals',
-//            ({ body }) => {
-//              hospitals.value = JSON.parse(body)
-//             //totalPages.value = Math.ceil(hospitals.value.length/ pageSize.value)
-//              emit('update-list', hospitals.value)
-//           }
-//       )
-//
-//       stompClient.value.subscribe('/topic/er-status', (message) => {
-//         const updated = JSON.parse(message.body)
-//         console.log('📦 병상 수 갱신 수신:', updated)
-//         updateHospitalInList(updated)
-//       })
-//
-//
-//       // 위치 전송용 토픽
-//
-//       if(navigator.geolocation){
-//         navigator.geolocation.watchPosition(
-//           ({coords}) => {
-//
-//             const lat = coords.latitude
-//             const lon = coords.longitude
-//
-//             position.value = {lat, lon}
-//
-//             trySendLocation(lat, lon)
-//           },
-//           err =>  console.error('위치 추적 오류:', err),
-//           {enableHighAccuracy : true, maximumAge: 5000}
-//         )
-//       }
-//     },
-//     (error) => {
-//       console.error('❌ STOMP 연결 실패:', error)
-//     }
-//   )
-// }
-// // 병상 수 갱신 로직
-// function updateHospitalInList(updatedHospital){
-//   const idx = hospitals.value.findIndex(h => h.id === updatedHospital.id)
-//   if(idx !== -1){
-//     hospitals.value[idx].availableErCount = updatedHospital.availableErCount
-//   }
-// }
-//
-//
-// // 초기 위치 조회 및 전송 + rest 호출
-// function locateMe() {
-//   console.log('[locateMe] 호출됨')
-//   if( !('geolocation' in navigator)){
-//       alert('브라우저에서 Geolocation을 지원하지 않습니다.')
-//     return
-//   }
-//
-//
-//       // granted, prompt
-//   return new Promise((resolve, reject) => {
-//     navigator.geolocation.getCurrentPosition(
-//       async ({coords}) => {
-//         const lat = coords.latitude
-//         const lon = coords.longitude
-//
-//
-//         position.value = {lat, lon}
-//
-//         // 검색어 초기화
-//         searchKeyword.value = ''
-//         currentPage.value = 0
-//
-//
-//         // 최초 한 번 rest로 데이터 받아옴
-//         await fetchHospitals('')
-//
-//         // rest 후에 stomp 연결
-//         connectWebSocket()
-//
-//         // 최초 위치도 stomp로 보냄 (10초 대기일 수 있으니까...)
-//         stompClient.value.publish({
-//           destination: '/publish/location',
-//           headers: {Authorization: 'Bearer ' + token},
-//           body: JSON.stringify({lat, lon})
-//         })
-//
-//
-//         // distance값이 유효하게 내려오는지 확인
-//         await fetchHospitals('')
-//         emit('find-location', { lat, lon })
-//
-//         resolve()
-//       },
-//       err => {
-//         console.error('초기 위치 조회 실패:', err)
-//         alert('위치 정보를 가져올 수 없습니다.')
-//       },
-//       {enableHighAccuracy: true}
-//     )
-//
-//
-//   })
-//
-// }
-//
-// // 현재 위치 가져옴
-// // function getCurrentPosition() {
-// //   navigator.geolocation.getCurrentPosition(
-// //     ({ coords }) => {
-// //       position.value.lat = coords.latitude
-// //       position.value.lon = coords.longitude
-// //
-// //       emit('find-location', {lat: coords.latitude, lon : coords.longitude})
-// //     },
-// //     (err) => {
-// //       console.error('위치 조회 실패:', err.message)
-// //       alert('위치 정보를 가져오지 못했습니다.'),
-// //         {enableHighAccuracy: true}
-// //     }
-// //   )
-// // }
-//
-// // 병원 데이터 호출
-// async function fetchHospitals(keyword) {
-//
-//   if(typeof keyword === 'string'){
-//     searchKeyword.value =  keyword.trim()
-//     currentPage.value = 0
-//   }
-//
-//
-//   try {
-//     const res = await apiClient.post(
-//       '/hospitals/search',
-//       null,
-//       {
-//         params: {
-//           lat:        position.value.lat,
-//           lon:        position.value.lon,
-//           sortOption: sortBy.value,
-//           keyword:    searchKeyword.value || undefined,
-//         }
-//       }
-//     )
-//
-//     console.log('✅ [fetchHospitals] 요청 URL   :', res.config.baseURL + res.config.url)
-//     console.log('✅ [fetchHospitals] 요청 params:', res.config.params)
-//     //hospitals.value = res.data || []
-//     //totalPages.value = Math.ceil(hospitals.value.length/pageSize.value)
-//
-//     hospitals.value = (res.data || []).map(h => ({
-//            ...h,
-//            distanceKm: Number(h.distanceKm),
-//            durationSec: Number(h.durationSec)
-//     }))
-//
-//     emit('update-list', hospitals.value)
-//     // 디버그 로그
-//     console.log('서버에서 받은 전체 병원 수:', hospitals.value.length)
-//     console.log('총 페이지 수:', totalPages.value)
-//
-//
-//
-//   } catch (err) {
-//     console.error('❌ [fetchHospitals] 에러 상태:', err.response?.status)
-//     console.error('❌ [fetchHospitals] 에러 메시지:', err.response?.data)
-//     hospitals.value = []
-//     //totalPages.value = 0
-//   }
-// }
-//
-// // 검색창에 키워드 있는 경우 처리
-// // function onSearch(keyword){
-// //   searchKeyword.value = keyword
-// //   currentPage.value = 0
-// //   fetchHospitals()
-// // }
-//
-// function changeSort(option) {
-//   if (SORT_OPTIONS.includes(option)) {
-//     sortBy.value = option
-//     currentPage.value = 0
-//     fetchHospitals()
-//   }
-// }
-//
-// function prevPage() {
-//   if (currentPage.value > 0) {
-//     currentPage.value--
-//     fetchHospitals()
-//   }
-// }
-//
-// function changePage(p) {
-//   if (p < 0 || p >= totalPages.value) return
-//   currentPage.value = p
-//   fetchHospitals()
-// }
-//
-// function  nextPage() {
-//   if(currentPage.value < totalPages.value - 1) {
-//     currentPage.value++
-//     fetchHospitals()
-//   }
-// }
-//
-//
-//
-//
-//
-// defineExpose({ fetchHospitals, hospitals, searchKeyword, currentPage, locateMe })
+
 </script>
 
 <style scoped>
